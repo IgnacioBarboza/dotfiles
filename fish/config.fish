3a1bb046882954ae77944422579a9e9c37f7e184
Ignacio Gonzalez Barboza <gobanacho@gmail.com>
2026-06-03 23:27:22 -0300

    feat(fish):definir fish por defecto y agregar alias para comandos de git

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
+
+# Atajo definitivo para VS Code nativo en Wayland y con pathing automático
+function code
+    command code --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations $argv
+end
+
+# Escribir 'y' en la terminal abrirá el gestor de archivos Yazi al instante
+alias y="yazi"
+
+# Atajos rápidos para Git (Para no escribir tanto comando largo)
+alias gst="git status"
+alias gadd="git add ."
+alias gcm="git commit -m"
+alias gpush="git push"
